1ad0942423d6eaff666ad7dafe37f813c548975e
Niramay Gogate <niramaygogate@gmail.com>
2024-02-23 23:32:22 -0600

    hcalnano production for LED phase scans. (Not finalized)

--- MakeNano.sh
+++ MakeNano.sh
@@ -5,5 +5,8 @@
 
 # time cmsDriver.py step2  -s RAW2DIGI,RECO,USER:DPGAnalysis/HcalNanoAOD/hcalNano_cff.hcalNanoTask --conditions auto:run3_data_prompt -n 10 --era Run3 --geometry DB:Extended --datatier NANOAOD --eventcontent NANOAOD --filein $1 --fileout file:$2
 
-# John's solution
-time cmsDriver.py step2  -s RAW2DIGI,RECO,USER:DPGAnalysis/HcalNanoAOD/hcalNano_cff.hcalNanoTask --conditions auto:run3_data_prompt -n 10 --era Run3 --geometry DB:Extended --datatier NANOAOD --eventcontent NANOAOD --customise_commands="process.load('DPGAnalysis.HcalNanoAOD.hcalUMNioTable_cff')\nprocess.hcalNanoTask.add(process.uMNioTable)\nprocess.hcalNanoDigiTask.add(process.uMNioTable)" --filein $1 --fileout file:$2
+# # John's solution
+# time cmsDriver.py step2  -s RAW2DIGI,RECO,USER:DPGAnalysis/HcalNanoAOD/hcalNano_cff.hcalNanoTask --conditions auto:run3_data_prompt -n 10 --era Run3 --geometry DB:Extended --datatier NANOAOD --eventcontent NANOAOD --customise_commands="process.load('DPGAnalysis.HcalNanoAOD.hcalUMNioTable_cff')\nprocess.hcalNanoTask.add(process.uMNioTable)\nprocess.hcalNanoDigiTask.add(process.uMNioTable)" -n -1 --filein $1 --fileout file:$2
+
+# For LED runs.
+cmsDriver.py HCALNANO --processName=HCALNANO -s RAW2DIGI,USER:DPGAnalysis/HcalNanoAOD/hcalNano_cff.hcalNanoDigiTask --datatier NANOAOD --eventcontent NANOAOD --filein $1 --fileout $2 --nThreads 4 --conditions auto:run3_data_prompt --era Run3 --python_filename testlocal_cfg.py --customise DPGAnalysis/HcalNanoAOD/hcalNano_cff.customiseHcalLocal
