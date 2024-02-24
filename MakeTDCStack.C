f1123ea49a60430f5488a335ce7ce9da7aad4ed5
GNiramay <niramaygogate@gmail.com>
2024-02-23 22:46:36 -0600

    The code is converted to python anyway

--- MakeTDCStack.C
+++ MakeTDCStack.C
@@ -5,45 +5,121 @@
 #include<iostream>
 #include"TCanvas.h"
 #include"TH2F.h"
+#include"TH1F.h"
 #include"THStack.h"
 #include"TFile.h"
 using namespace std;
+// THStack* Trial1(TH2F*);
+THStack* Trial2(TH2F*);
+TString TDCInfo[4] = {"Prompt","Slight delay","Delayed","No TDC"};
 
 int main(int argc,char** argv)
 {
-  // cout<<argv[1]<<"\t"<<argv[2]<<endl;
   TFile* tf = new TFile(argv[1],"READ");
   TH2F* hist = (TH2F*)tf->Get("h2TDCvTShift");
-  // hist->Print("all");
-  THStack* hs = new THStack();
-  // auto hStat = hist->ProjectionX();
-  // auto hStat = hist->ProjectionX("hStat",0,5);
-  // hStat->Print("all");
-  for(int i=0;i<4;i++){
-    auto htemp = hist->ProjectionX(Form("hTDC%d",i),i+1,i+2);
-    htemp->SetTitle(Form("TDC %d; time shift [ns]",i));
-    htemp->SetFillColor(i+2);
-    htemp->SetLineColor(1);
-    // htemp->Print("all");
+  // THStack* hs = new THStack();
+  // TH1F* hTDC[4] = {
+  //   new TH1F("hTDC0","",32,-10.5,21.5),
+  //   new TH1F("hTDC1","",32,-10.5,21.5),
+  //   new TH1F("hTDC2","",32,-10.5,21.5),
+  //   new TH1F("hTDC3","",32,-10.5,21.5)};
+  
+  // int nxbins = hist->GetNbinsX();
+  // int nybins = hist->GetNbinsY();
+  // Double_t* nTotEvt = new Double_t[nxbins];
 
-    // Scale each shift
-    for(int j = 1;j<htemp->GetNbinsX()-1;j++){
-      Double_t nTotEvt = 0;
-      for(int k=01;k<5;k++) nTotEvt += hist->GetBinContent(j,k);
-      cout<<j<<"\t"<<nTotEvt<<"\t"<<htemp->GetBinContent(j)<<endl;
-      // htemp->SetBinContent(j,0);
-      if(nTotEvt !=0) htemp->SetBinContent(j,htemp->GetBinContent(j)/nTotEvt);
-      // if(nTotEvt !=0) htemp->Fill(j,htemp->GetBinContent(j)/nTotEvt);
-      cout<<j<<"\t"<<nTotEvt<<"\t"<<htemp->GetBinContent(j)<<endl;
-      //   if(hStat->GetBinContent(j) != 0)
-      // 	htemp->SetBinContent(j,htemp->GetBinContent(j)/hStat->GetBinContent(j));
-    }
-    hs->Add(htemp);    
-  }
+  // for(int bx = 1;bx<=nxbins;bx++){
+  //   Double_t temp = 0;
+  //   for(int by = 1;by<=nybins;by++)
+  //     temp += hist->GetBinContent(hist->GetBin(bx,by));
+  //   nTotEvt[bx-1] = temp;
+  // }
+
+  // for(int by = 1;by<=nybins;by++){
+  //   hTDC[by-1]->SetTitle(Form("TDC %d (%s); time shift [ns]",by-1,TDCInfo[by-1].Data()));
+  //   hTDC[by-1]->SetFillColor(by+1);
+  //   hTDC[by-1]->SetLineColor(1);
+  //   for(int bx = 1;bx<=nxbins;bx++){
+  //     if(nTotEvt[bx-1]!=0)
+  // 	hTDC[by-1]->SetBinContent(bx-1,hist->GetBinContent(hist->GetBin(bx,by))/nTotEvt[bx-1]);
+  //   }
+  //   hs->Add(hTDC[by-1]);
+  // }
+
+  auto hs = Trial2(hist);
 
   TCanvas* tc = new TCanvas("aa","bb",800,600);
   hs->Draw("hist");
-  hs->SetTitle(hist->GetTitle());
-  tc->BuildLegend(.8,.7,.9,.9);
+  // Some formatting
+  hs->SetTitle(""+(TString)hist->GetTitle()+";time shift [ns];fraction of events");
+  tc->BuildLegend(.7,.7,.9,.9);
+  hs->SetMaximum(1);
+
+  // Save png
   tc->SaveAs(argv[2]);
+  delete tc;
+  delete hs;
+  tf->Close();
+  return 0;
+}
+
+// THStack* Trial1(TH2F* hist)
+// {
+//   THStack* hs = new THStack();
+//   TH1F* hTDC[4] = {
+//     new TH1F("hTDC0","",32,-10.5,21.5),
+//     new TH1F("hTDC1","",32,-10.5,21.5),
+//     new TH1F("hTDC2","",32,-10.5,21.5),
+//     new TH1F("hTDC3","",32,-10.5,21.5)};
+  
+//   int nxbins = hist->GetNbinsX();
+//   int nybins = hist->GetNbinsY();
+//   Double_t* nTotEvt = new Double_t[nxbins];
+
+//   for(int bx = 1;bx<=nxbins;bx++){
+//     Double_t temp = 0;
+//     for(int by = 1;by<=nybins;by++)
+//       temp += hist->GetBinContent(hist->GetBin(bx,by));
+//     nTotEvt[bx-1] = temp;
+//   }
+
+//   for(int by = 1;by<=nybins;by++){
+//     hTDC[by-1]->SetTitle(Form("TDC %d (%s); time shift [ns]",by-1,TDCInfo[by-1].Data()));
+//     hTDC[by-1]->SetFillColor(by+1);
+//     hTDC[by-1]->SetLineColor(1);
+//     for(int bx = 1;bx<=nxbins;bx++){
+//       if(nTotEvt[bx-1]!=0)
+// 	hTDC[by-1]->SetBinContent(bx-1,hist->GetBinContent(hist->GetBin(bx,by))/nTotEvt[bx-1]);
+//     }
+//     hs->Add(hTDC[by-1]);
+//   }
+//   return hs;
+// }
+
+THStack* Trial2(TH2F* hist_)
+{
+  THStack* hs_ = new THStack();
+  auto hTotEvt = hist_->ProjectionX();
+  int nxbins = hist_->GetNbinsX();
+  int nybins = hist_->GetNbinsY();
+
+  TH1F* hTDC[4] = {
+    new TH1F("hTDC0","",nxbins,hTotEvt->GetBinLowEdge(0),hTotEvt->GetBinLowEdge(nxbins)),
+    new TH1F("hTDC1","",nxbins,hTotEvt->GetBinLowEdge(0),hTotEvt->GetBinLowEdge(nxbins)),
+    new TH1F("hTDC2","",nxbins,hTotEvt->GetBinLowEdge(0),hTotEvt->GetBinLowEdge(nxbins)),
+    new TH1F("hTDC3","",nxbins,hTotEvt->GetBinLowEdge(0),hTotEvt->GetBinLowEdge(nxbins))};
+  
+
+  for(int by = 1;by<=nybins;by++){
+    hTDC[by-1]->SetTitle(Form("TDC %d (%s); time shift [ns]",by-1,TDCInfo[by-1].Data()));
+    hTDC[by-1]->SetFillColor(by+1);
+    hTDC[by-1]->SetLineColor(1);
+
+    for(int bx = 0;bx<nxbins;bx++)
+      if(hTotEvt->GetBinContent(bx+1)!=0)
+	hTDC[by-1]->SetBinContent(bx,hist_->GetBinContent(hist_->GetBin(bx+1,by))/hTotEvt->GetBinContent(bx+1));
+
+    hs_->Add(hTDC[by-1]);
+  }
+  return hs_;
 }
