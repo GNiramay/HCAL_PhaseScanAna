fcd837498ebcd10eea44252df0cf05ae980843cf
Niramay Gogate <niramaygogate@gmail.com>
2024-03-22 17:11:30 -0500

    Few obvious changes. Mostly, not gonna use this file

--- PlotTDCFrac.py
+++ PlotTDCFrac.py
@@ -2,7 +2,9 @@
 # Example: python3 PlotTDCFrac.py TestSkimmed.root TestHist.root hb 15 2
 
 from sys import argv
-from ROOT import RDataFrame, TFile
+from ROOT import RDataFrame, TFile, EnableImplicitMT
+
+EnableImplicitMT()
 
 InRoot = argv[1]
 OutRoot = argv[2]
@@ -12,15 +14,22 @@ depth_ = argv[5]
 SOI = 3                         # Sample of interest
 
 df = RDataFrame('Events',InRoot)
-TShift = df.Max('tshift').GetValue()
+# TShift = df.Max('tshift').GetValue()
 
-hist = df.Define('MyChannel',f"{Det_}_ieta == {eta_} && {Det_}_depth == {depth_}")\
-         .Define('MyTDC',f"{Det_}_tdc{SOI}[MyChannel]")\
-         .Define('MyShift',f"vector<Double_t>temp; for(auto cc: MyTDC) temp.push_back(tshift); return temp;")\
-         .Histo2D(("h2TDCvTShift",f"{Det_} i#eta {eta_} | depth {depth_} | ts {SOI};phase shift [ns];TDC",32,-10.5,21.5,4,-0.5,3.5),'MyShift','MyTDC')
+df = df.Define('MyChannel',f"{Det_}_ieta == {eta_} && {Det_}_depth == {depth_}")\
+       .Define('MyTDC',f"{Det_}_tdc{SOI}[MyChannel]")\
+       .Define('MyShift',f"vector<Double_t>temp; for(auto cc: MyTDC) temp.push_back(tshift); return temp;")
+
+hist = df.Histo2D(("h2TDCvTShift",f"{Det_} i#eta {eta_} | depth {depth_} | ts {SOI};phase shift [ns];TDC",32,-10.5,21.5,4,-0.5,3.5),'MyShift','MyTDC')
+hist2 = df.Histo1D(("hMyShift",";phase shift according to MyShift",34,-12,22),"MyShift")
+hist3 = df.Histo1D(("htshift",";phase shift according to tshift",34,-12,22),"tshift")
+hist4 = df.Histo1D(("hMyTDC",f"{Det_} i#eta {eta_} | depth {depth_} | ts {SOI};TDC",4,-0.5,3.5),"MyTDC")
 
 tf = TFile(OutRoot,"RECREATE")
 hist.Write()
+hist2.Write()
+hist3.Write()
+hist4.Write()
 tf.Write()
 tf.Close()
-         # .Define('MyShift',f"{TShift} + 0*MyTDC")\
+# .Define('MyShift',f"{TShift} + 0*MyTDC")\
