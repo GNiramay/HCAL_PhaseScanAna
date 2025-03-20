3fcbb4a24e19760d5c8825ca78688fde96e00641
GNiramay <niramaygogate@gmail.com>
2025-03-20 16:11:56 +0100

    Including analysis script name in the output root file

--- Exec/GeneralAna.sh
+++ Exec/GeneralAna.sh
@@ -8,7 +8,8 @@ ls -alht
 echo Input arguments: $*
 AnaScript=$1
 InRoot=$2
-OutRoot=$(basename $InRoot)
+AnaTag=$(echo $AnaScript | sed 's/.py//')
+OutRoot=${AnaTag}_$(basename $InRoot)
 
 time python3 $AnaScript $InRoot $OutRoot
 ls -alht
