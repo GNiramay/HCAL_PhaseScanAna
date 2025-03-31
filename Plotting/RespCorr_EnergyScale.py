3da9b0c054c6be041b870956fb9d6c86594fe6e9
GNiramay <niramaygogate@gmail.com>
2025-03-31 18:23:40 +0200

    Calculating and storing the response corrections

--- Plotting/RespCorr_EnergyScale.py
+++ Plotting/RespCorr_EnergyScale.py
@@ -1,13 +1,22 @@
 # Program to find an approximate energy threshold required to reach the given occupancy (https://cds.cern.ch/record/2891496 page 21, fig 17 a)
+# Arguments: <histogram root file> <histogram name> <RefVal text file> <output folder>
 import ROOT as rt
+import numpy as np
 from sys import argv
 from array import array as ar
 rt.gROOT.SetBatch(True)
 rt.gStyle.SetOptStat(0)
 rt.gStyle.SetOptTitle(0)
 
-# Folder to save plots
-OutFolder = '/eos/home-n/ngogate/www/HCAL_DPG/PhaseScan_2024/'
+tf = rt.TFile(argv[1],'READ')
+hAll = tf.Get(argv[2])
+# hAll = tf.Get('hRespCorrData')
+InTXT = argv[3]
+OutFolder = argv[4]
+# OutFolder = '/eos/home-n/ngogate/www/HCAL_DPG/PhaseScan_2024/'
+
+# root file to store the resp. corrections.
+OutROOT = InTXT.split('.txt')[0]+'.root'
 
 # 2024 phase scan lumi info
 LumiInfo = {"379349":46.48,
@@ -17,20 +26,19 @@ LumiInfo = {"379349":46.48,
 # Get hits over 4GeV
 Rechit_energy_thr = 4.0
 
-tf = rt.TFile(argv[1],'READ')
-hAll = tf.Get('hRespCorrData')
-
 # Get total no. of bins along all axes
 nTotalBins = [hAll.GetAxis(ii).GetNbins() for ii in range(hAll.GetNdimensions())]
 
 # A dictionary of histograms for effective plotting
 hNewScale = {}
 hNewOccup = {}
+
 for ee in range(-29,30):
     for dd in range(1,8):
-        hNewScale[f'{ee}_{dd}'] = rt.TH1F(f'hNewScale_{ee}_{dd}',f'depth {dd};QIE Phase Offset [ns];Energy Scale',11,-6.5,4.5)
-        hNewOccup[f'{ee}_{dd}'] = rt.TH1F(f'hNewOccup_{ee}_{dd}',f'depth {dd};QIE Phase Offset [ns];Fraction of hits over scaled 4 GeV',11,-6.5,4.5)
-
+        # hNewScale[f'{ee}_{dd}'] = rt.TH1F(f'hNewScale_{ee}_{dd}',f'depth {dd};QIE Phase Offset [ns];Energy Scale',11,-6.5,4.5)
+        # hNewOccup[f'{ee}_{dd}'] = rt.TH1F(f'hNewOccup_{ee}_{dd}',f'depth {dd};QIE Phase Offset [ns];Fraction of hits over scaled 4 GeV',11,-6.5,4.5)
+        hNewScale[f'{ee}_{dd}'] = rt.TH1F(f'hNewScale_{ee}_{dd}',f'depth {dd};QIE Phase Offset [ns];Energy Scale',31,-10.5,20.5)
+        hNewOccup[f'{ee}_{dd}'] = rt.TH1F(f'hNewOccup_{ee}_{dd}',f'depth {dd};QIE Phase Offset [ns];Fraction of hits over scaled 4 GeV',31,-10.5,20.5)
 
 def DrawCMSHeading(opt=1,lumi_pb=300,ycoord=0.91,xleft=0.12,xright=0.7):
     textOnTop = rt.TLatex()
@@ -46,13 +54,8 @@ def DrawCMSHeading(opt=1,lumi_pb=300,ycoord=0.91,xleft=0.12,xright=0.7):
     intLumiE.DrawLatexNDC(xright,ycoord,"#bf{"+str(lumi_pb)+" pb^{-1} (13 TeV)}")
     return
 
-# def PlotEnergyScale(IEta,refVal,OutNameTag):
-#     return
-
-with open('RefVal.txt','r') as f:
-
-    # # This will store the energy scales, and the occupancy at the new scales
-    # NewData = []
+# with open('RefVal.txt','r') as f:
+with open(InTXT,'r') as f:
 
     # Loop over each entry (one ieta ring)
     for gg in f.readlines():
@@ -74,9 +77,6 @@ with open('RefVal.txt','r') as f:
 
             nevt = hist.Integral(0,nTotalBins[3]+1)
             if nevt ==0: continue
-            # print(ieta,depth,timeshift,nevt,ref_occup)
-            # hist.Print("all")
-            # print(ieta,depth,timeshift,ref_occup)
             
             bin_id = nTotalBins[3]+1
             Occup = hist.GetBinContent(bin_id)/nevt
@@ -89,10 +89,42 @@ with open('RefVal.txt','r') as f:
 
             hNewScale[f'{ieta}_{depth}'].Fill(timeshift,NewThr/Rechit_energy_thr)
             hNewOccup[f'{ieta}_{depth}'].Fill(timeshift,Occup)
-            # NewData.append([timeshift,ieta,depth,NewThr/Rechit_energy_thr,Occup])
-            # if ieta == 2 and depth == 4:
-            #     print([timeshift,ieta,depth,NewThr/Rechit_energy_thr,Occup])
-    # print(NewData)
+
+def WriteRespCorr():
+    # Prepare root file, TTree to store the values
+    OutFile = rt.TFile(OutROOT,'RECREATE')
+    OutTree = rt.TTree('RespCorrData','compensating response corrections')
+    Data = [np.array([0],np.int_),
+            np.array([0],np.int_),
+            np.array([0],np.int_),
+            np.array([0],np.int_),
+            np.array([0],np.float64)]
+    
+    for ii,vv in enumerate([['ieta','I'],
+                            ['depth','I'],
+                            ['InitialPhase','I'],
+                            ['FinalPhase','I'],
+                            ['RespCorr','D']]):
+        OutTree.Branch(vv[0],Data[ii],vv[0]+'/'+vv[1])
+
+    # Loop over histograms for each ieta ring
+    for nn,hh in hNewScale.items():
+        if hh.GetEntries() == 0: continue
+        ieta_,depth_ = nn.split('_')
+        Data[0][0] = int(ieta_)
+        Data[1][0] = int(depth_)
+        nBins = hh.GetNbinsX()
+        for ii in range(nBins):
+            Data[2][0] = hh.GetBinCenter(ii+1)
+            for jj in range(ii,nBins):
+                if hh.GetBinContent(jj+1) == 0: continue
+                Data[3][0] = hh.GetBinCenter(jj+1)
+                Data[4][0] = hh.GetBinContent(ii+1)/hh.GetBinContent(jj+1)
+                OutTree.Fill()
+
+    OutTree.Write()
+    OutFile.Close()
+    return
 
 # Plot occupancy
 for ee in range(-29,30):
@@ -147,3 +179,5 @@ for ee in range(-29,30):
     tc.SaveAs(OutFolder+f'NewScale/IEta_{ee}.png')
     tc.SaveAs(OutFolder+f'NewScale/IEta_{ee}.pdf')
     del tc
+
+WriteRespCorr()
